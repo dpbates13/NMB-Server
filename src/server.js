630cf65cb69a88944ea2e983307ac9a6980b1c95
Dustin Bates <dpbates13@gmail.com>
2020-07-11 15:26:31 -0500

    port

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 const { PORT } = require("./config");
 
-app.listen(PORT, () => {
+app.listen(40565, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
 });
