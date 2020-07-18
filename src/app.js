ddebd63e507f007fd7f222195fd770ca9d7658fe
Dustin Bates <dpbates13@gmail.com>
2020-07-18 12:06:41 -0500

    fix

--- src/app.js
+++ src/app.js
@@ -159,7 +159,7 @@ function createGenreList() {
   }
 }
 
-app.get("*", async (req, res) => {
+app.get("/data", async (req, res) => {
   await getToken();
   await createAlbumDatabase(url);
   await getArtists(createArtistStrings(albumDatabase));
